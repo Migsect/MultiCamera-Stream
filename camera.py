3af90651cf50b8fda34c9011d5837a0e6b7cb11e
Alex Sammons <alsamong@gmail.com>
2020-07-12 03:12:52 -0400

    Added proper per camera threading
    Also added a run.bat for laziness I guess

--- camera.py
+++ camera.py
@@ -1,41 +1,151 @@
 import cv2
+import threading
+from queue import Queue, Empty
+from readerwriterlock import rwlock
+import time
 
 THUMBAIL_SIZE = 120
 
-devices = {}
+
+def resize_to_resolution(image, resolution):
+    # We are going to resize the image into one we see fit.
+    height = int(resolution)
+    width = int(image.shape[1] * height / image.shape[0])
+    dims = (width, height)
+    return cv2.resize(
+        image, dims,
+        interpolation=cv2.INTER_AREA
+    )
+
+
+class DataQueue:
+    def __init__(self):
+        self.queue = Queue(maxsize=16)
+
+        self.current_data = None
+        self.lock = rwlock.RWLockFair()
+
+    def update_data(self):
+        next_data = None
+        try:
+            next_data = self.queue.get(block=False)
+        except Empty:
+            # There is no new data to update.
+            return None
+        with self.lock.gen_wlock():
+            self.current_data = next_data
+
+    def get_data(self):
+        result = None
+        with self.lock.gen_rlock():
+            result = self.current_data
+
+        # We are going to try to update the data if there is any new data in the queue.
+        # It is the reader's responsibility to update the data being held if there is new data.
+        self.update_data()
+
+        return result
+
+    def add_data(self, data):
+        if self.queue.full():
+            try:
+                # emptying one of the items in the queue if its full.
+                self.update_data()
+            except Empty:
+                pass
+
+        self.queue.put(data)
 
 
 class VideoCamera(object):
-    def __init__(self, device=0, resolution=1080):
-        if device in devices:
-            self.video = devices[device]
-        else:
-            self.video = cv2.VideoCapture(device)
-            devices[device] = self.video
+    def __init__(self, configuration):
+        self.camera_id = configuration["id"]
+        self.camera_name = configuration["name"]
+        self.device = configuration["device-number"]
+        self.resolution = configuration.get("resolution", 1080)
+        self.pollrate = configuration.get("pollrate", 10)
+        self.polltime = 1 / self.pollrate
 
-        self.resolution = resolution
+        # self._video = None
+        self._video = cv2.VideoCapture(self.device, cv2.CAP_DSHOW)
 
     def __del__(self):
-        self.video.release()
-
-    def generate(self, thumbnail=False):
-        while True:
-            frame = self.get_frame(thumbnail=thumbnail)
-            yield (
-                b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
-            )
+        self._video.release()
 
     def get_frame(self, thumbnail=False):
-        success, image = self.video.read()
 
-        height = int(self.resolution if not thumbnail else THUMBAIL_SIZE)
-        width = int(image.shape[1] * height / image.shape[0])
-        dims = (width, height)
-        image = cv2.resize(image, dims,
-                           interpolation=cv2.INTER_AREA)
+        retval, image = self._video.read()
+        if retval != True:
+            print("Camera can't read frame: " + self.camera_id)
+
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        return image
+
+
+class VideoCameraThread(threading.Thread):
+    def __init__(self, camera_config):
+
+        self.camera = VideoCamera(camera_config)
+
+        threading.Thread.__init__(
+            self, name=self.camera.camera_id, daemon=True)
+
+        self.data_queue = DataQueue()
+
+    def run(self):
+        print("Starting video camera thread for " + self.camera.camera_id)
+
+        data = self.camera.get_frame()
+        self.data_queue.add_data(data)
+
+        count = 0
+        while True:
+            seconds = time.time()
+
+            count += 1
+
+            data = self.camera.get_frame()
+
+            if data is None:
+                print("Video stream invalid for " +
+                      self.camera.camera_id + ", stopping thread.")
+                break
+
+            self.data_queue.add_data(data)
+
+            difference = time.time() - seconds
+            if difference < self.camera.polltime:
+                time.sleep(self.camera.polltime - difference)
+
+
+class VideoCameraConsumer(object):
+    def __init__(self, thread, is_thumbnail):
+        self.thread = thread
+        self.is_thumbnail = is_thumbnail
+
+    def generate(self):
+        while True:
+            seconds = time.time()
+
+            frame = self.thread.data_queue.get_data()
+            if frame is None:
+                print("frame is none")
+                continue
+
+            if self.is_thumbnail:
+                frame = resize_to_resolution(frame, THUMBAIL_SIZE)
+            else:
+                frame = frame = resize_to_resolution(
+                    frame, self.thread.camera.resolution)
+
+            _, jpeg = cv2.imencode('.jpg', frame)
+            yield (
+                b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n'
+            )
+
+            difference = time.time() - seconds
+            if difference < self.thread.camera.polltime:
+                time.sleep(self.thread.camera.polltime - difference)
--- main.py
+++ main.py
@@ -1,43 +1,38 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response, request, redirect, url_for
-from camera import VideoCamera
+from camera import VideoCameraThread, VideoCamera, VideoCameraConsumer
 import json
 
 app = Flask(__name__)
 
 cameras = []
-camera_map = {}
 
 
 def parse_config():
     with open('config.json') as json_file:
         data = json.load(json_file)
         cameras.extend(data.get("cameras", []))
-        for index, camera in enumerate(cameras):
-            camera_id = camera.get("id", "camera_" + str(index))
-            camera_map[camera_id] = camera
-            camera["video"] = VideoCamera(
-                device=camera["device-number"],
-                resolution=camera["resolution"]
-            )
 
 
-parse_config()
-print(cameras)
+camera_threads = []
+camera_threads_map = {}
+
+
+def start_threads():
+    print(cameras)
+    for camera in cameras:
+        # v_camera = VideoCamera(camera)
+        v_camera_thread = VideoCameraThread(camera)
+        camera_threads.append(v_camera_thread)
+        camera_threads_map[v_camera_thread.camera.camera_id] = v_camera_thread
+
+        print("Starting Thread")
+        v_camera_thread.start()
 
 
 @app.route('/')
 def index():
     return render_template('index.html', cameras=cameras)
-
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (
-            b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
-        )
 
 
 @app.route('/video_feed/')
@@ -49,10 +44,16 @@ def video_feed():
 
     print(camera_id, is_thumbnail)
     return Response(
-        camera_map[camera_id]["video"].generate(thumbnail=is_thumbnail),
+        VideoCameraConsumer(
+            camera_threads_map[camera_id], is_thumbnail).generate(),
         mimetype='multipart/x-mixed-replace; boundary=frame'
     )
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+
+    parse_config()
+    print("Starting All Threads")
+    start_threads()
+
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
